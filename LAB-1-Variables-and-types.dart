ad583a7c8e0bd861476034b900614d2a7d3b72fd
Hanim07 <148555136+Hanim07@users.noreply.github.com>
2024-03-08 14:55:39 +0300

    Create LAB-1-Variables-and-types.dart

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Hanim";
+  int age = 19;
+  String favoriteColor = "black";
+
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $favoriteColor.");
+}
