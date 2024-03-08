47afa292d4cf3bc9bf2a120d4630da6f6c38ba3b
Hanim07 <148555136+Hanim07@users.noreply.github.com>
2024-03-08 17:24:04 +0300

    Create LAB-17-Asynchronous-Programming.dart

---
+++ LAB-17-Asynchronous-Programming.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+Future<List<String>> fetchDataFromDatabase() async {
+  await Future.delayed(
+      Duration(seconds: 2)); 
+  return ["Data 1", "Data 2", "Data 3"];
+}
+
+void main() async {
+  print("Loading data...");
+  List<String> data = await fetchDataFromDatabase();
+  print("Data loaded: $data");
+}
