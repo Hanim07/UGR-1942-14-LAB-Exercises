30691b62808b1632c906ef5fd6e01ba39012d770
Hanim07 <148555136+Hanim07@users.noreply.github.com>
2024-05-17 15:27:29 +0300

    Create Lab-8-kotlin-functions.kt

---
+++ Lab-8-kotlin-functions.kt
@@ -0,0 +1,20 @@
+fun hasUniqueCharacters(str: String): Boolean {
+    val charSet = mutableSetOf<Char>()
+    
+    for (char in str) {
+        if (charSet.contains(char)) {
+            return false
+        }
+        charSet.add(char)
+    }
+    
+    return true
+}
+
+fun main() {
+    val testString1 = "abcdef"
+    val testString2 = "78llo"
+    
+    println("Does '$testString1' have all unique characters? ${hasUniqueCharacters(testString1)}")
+    println("Does '$testString2' have all unique characters? ${hasUniqueCharacters(testString2)}")
+}
